d7b6cf61c2ea44ad695aee4750e696cbaf4d70fa
codingmatemoon <abc9529@naver.com>
2022-08-22 07:13:57 +0900

    MemberServiceImpl가 MemberService 추상화에도 의존하고 MemoryMemberRepository 구체화에도 의존하는 문제. DIP 위반

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
+    // MemberServiceImpl은 MemberService 추상화에도 의존하고 MemoryMemberRepository 구체화에도 의존하고 있습니다. DIP 위반
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,28 @@
+package hello.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    @DisplayName("join")
+    void join() {
+        // given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        // when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        // then
+        assertThat(member).isEqualTo(findMember);
+    }
+
+
+}
