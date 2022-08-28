d030384d65e3c22ece8521c214f70360eaff6581
codingmatemoon <abc9529@naver.com>
2022-08-29 07:49:58 +0900

    추상화, 구체화 동시에 의존하는 코드를 추상화에만 의존하는 코드로 변경. 생성자를 통해서 객체를 주입. 생성자 주입

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,8 +2,17 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    // MemberServiceImpl은 MemberService 추상화에도 의존하고 MemoryMemberRepository 구체화에도 의존하고 있습니다. DIP 위반
+    private final MemberRepository memberRepository;
+
+    // 생성자를 통해서 객체를 주입. 생성자 주입
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /* MemberServiceImpl은 MemberService 추상화에도 의존하고 MemoryMemberRepository 구체화에도 의존하고 있습니다. DIP 위반
     private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+     */
 
     @Override
     public void join(Member member) {
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPoilcy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -10,7 +11,12 @@ public class OrderServiceImpl implements OrderService{
 
     // 주문 서비스는 저장소, 가격정책 2개가 필요합니다.
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPoilcy discountPoilcy = new FixDiscountPolicy();
+    private DiscountPoilcy discountPoilcy;
+    /* 가격 정책 변경
+    추상화, 구체화 둘 다 의존하는 경우에서 추상화에만 의존하는 경우로 변경
+    private final DiscountPoilcy discountPoilcy = new FixDiscountPolicy() -> private DiscountPoilcy discountPoilcy;
+
+     */
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
