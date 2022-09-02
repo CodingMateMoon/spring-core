a545163982b82bcc1ed7345d9c341e1fc4209506
codingmatemoon <abc9529@naver.com>
2022-09-03 00:15:52 +0900

    AppConfig 스프링 기반으로 변경. @Configuration 설정 정보 기재. 스프링은 ApplicationContext(스프링 컨테이너)로 시작해서 모든 객체들을 관리합니다. AppConfig에 있는 설정 정보를 가지고(@Bean) 스프링 컨테이너에 객체를 생성하고 등록한 뒤 관리합니다.

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,26 +9,34 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+// 애플리케이션의 구성정보 설정정보를 기재합니다.
+@Configuration
 public class AppConfig {
-    // AppConfig가 객체의 생성과 연결을 담당하며 MemberServiceImple은 MemberRepository인 추상에만 의존합니다. MemberServiceImple 객체, MemoryMemberRepository등을 생성해서 MemoryMemberRepository를 MemberServiceImpl에 넣어줍니다.
+    // AppConfig가 객체의 생성과 연결을 담당하며 MemberServiceImpl은 MemberRepository인 추상에만 의존합니다. MemberServiceImple 객체, MemoryMemberRepository등을 생성해서 MemoryMemberRepository를 MemberServiceImpl에 넣어줍니다.
 
     /* AppConfig에서 역할과 구현 클래스가 한 눈에 들어오도록 표현
     MemberService 역할(현재 MemberService는 MemberServiceImpl을 사용), MemberRepository 역할(현재 MemberRepository는 MemoryMemberRepository를 사용), OrderService 역할
      */
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPoilcy());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
     // 할인 정책을 변경해도 애플리케이션의 구성 역할을 담당하는 AppConfig만 변경하면 됩니다. 구성 역할을 담당하는 AppConfig 공연 기획자가 공연 참여자인 구현 객체들을 모두 알고 구성 영역을 변경시킵니다. DiscountPolicy 추상화에 의존하고 구현체를 받아서 쓰기 때문에 클라이언트 코드인 OrderServiceImpl 등 사용영역의 코드는 변경하지 않습니다.
+    @Bean
     public DiscountPoilcy discountPoilcy() {
 
 //        return new FixDiscountPolicy();
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,13 +3,22 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-//        MemberService memberService = new MemberServiceImpl(null);
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+        /*
+        스프링은 ApplicationContext(스프링 컨테이너)로 시작해서 모든 객체들을 관리합니다.
+        AppConfig에 있는 설정 정보를 가지고(@Bean) 스프링 컨테이너에 객체를 생성하고 등록한 뒤 관리합니다.
+         */
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // Config에 있는 메서드 이름 memberService, 반환 타입 MemberService.class 를 파라미터로 넘겨서 MemberService를 가져옵니다.
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
