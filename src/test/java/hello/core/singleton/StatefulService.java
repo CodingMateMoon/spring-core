9d4911f2584672727268cbb897169fab8a810b15
codingmatemoon <abc9529@naver.com>
2023-01-06 08:08:05 +0900

    스프링 빈을 stateful 상태 유지하게 설계할 경우 문제 코드 객체 인스턴스를 하나만 생성해서 공유하는 싱글톤 방식은 여러 클라이언트가 하나의 같은 객체 인스턴스를 공유하기 때문에 싱글톤 객체는 상태를 유지(stateful)하게 설계하면 안됩니다. 스프링 빈 필드에 공유 값 설정 시 장애가 발생할 수 있으므로 무상태(stateless) 설계가 필요합니다. - 특정 클라이언트 의존 필드X, 클라이언트가 필드값 변경X, 읽기만 가능, 필드 대신 공유되지 않는 지역변수, 파라미터, ThreadLocal 등 사용.

---
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -0,0 +1,15 @@
+package hello.core.singleton;
+
+public class StatefulService {
+    
+    private int price; // 상태를 유지하는 필드
+
+    public void order(String name, int price) {
+        System.out.println("name +  = " + name + " price = " + price);
+        this.price = price; // 여기가 문제! (주문해서 값 저장한 지점)
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
