3f27ab266ff5692fa73d59cbd2e35d150950c122
codingmatemoon <abc9529@naver.com>
2023-01-13 08:41:03 +0900

    [의존관계 주입] 필드 주입 - 외부에서 변경이 불가능해서 테스트하기 어렵고 DI 프레임워크가 없으면 할 수 있는 게 없습니다. 일반 메서드 주입 - 스프링 빈에서만 @Autowired를 적용시킬 수 있는데 한번에 여러 필드를 주입받을 수 있지만 잘 사용하지는 않습니다.

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -10,9 +10,24 @@ import org.springframework.stereotype.Component;
 public class OrderServiceImpl implements OrderService{
 
     // 주문 서비스는 저장소, 가격정책 2개가 필요합니다.
-    private final MemberRepository memberRepository;
-    private final DiscountPoilcy discountPoilcy;
+//    @Autowired private MemberRepository memberRepository;
+//    @Autowired private DiscountPoilcy discountPoilcy;
+     private MemberRepository memberRepository;
+     private DiscountPoilcy discountPoilcy;
 
+    public void setMemberRepository(MemberRepository memberRepository) {
+        System.out.println("OrderServiceImpl.setMemberRepository");
+        System.out.println("memberRepository = " + memberRepository);
+        this.memberRepository = memberRepository;
+    }
+
+    public void setDiscountPoilcy(DiscountPoilcy discountPoilcy) {
+        System.out.println("OrderServiceImpl.setDiscountPoilcy");
+        System.out.println("discountPoilcy = " + discountPoilcy);
+        this.discountPoilcy = discountPoilcy;
+    }
+
+    /*
     @Autowired
     public void setMemberRepository(MemberRepository memberRepository) {
         System.out.println("OrderServiceImpl.setMemberRepository");
@@ -26,6 +41,7 @@ public class OrderServiceImpl implements OrderService{
         System.out.println("discountPoilcy = " + discountPoilcy);
         this.discountPoilcy = discountPoilcy;
     }
+     */
 
     //@Autowired 생성자가 1개일 경우 @Autowired를 생략해도 가능합니다.
     //new OrderServiceImpl(memberRepository, discountPolicy);
@@ -36,6 +52,13 @@ public class OrderServiceImpl implements OrderService{
         this.memberRepository = memberRepository;
         this.discountPoilcy = discountPoilcy;
     }
+
+    @Autowired
+    public void init(MemberRepository memberRepository, DiscountPoilcy discountPoilcy) {
+        this.memberRepository = memberRepository;
+        this.discountPoilcy = discountPoilcy;
+    }
+
     /* 가격 정책 변경
     추상화, 구체화 둘 다 의존하는 경우에서 추상화에만 의존하는 경우로 변경. Memory, JDBC, JPA Repository 등 어떤 저장소가 오는 지 모릅니다.
     AppConfig를 통해 실제 동작에 필요한 구현 객체를 받아서 사용합니다.
