ed075b62ca72c1f33a68d2140fd1e661159056d9
codingmatemoon <abc9529@naver.com>
2023-01-23 09:47:52 +0900

    InitializingBean - afterPropertiesSet() 메서드로 초기화 지원 DisposableBean : destroy() 메서드로 소멸 지원

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,6 +1,9 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
@@ -26,5 +29,20 @@ public class NetworkClient {
     // 서비스 종료시 호출
     public void disconnect() {
         System.out.println("close : " + url);
+    }
+
+
+    // 의존관계 주입이 끝나면 호출해줍니다
+    @Override
+    public void afterPropertiesSet() {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception{
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
