389e49aabb3577755d1bceb839e83182f480ff9b
codingmatemoon <abc9529@naver.com>
2023-02-02 09:07:59 +0900

    spring 컨테이너가 CGLIB 바이트코드 조작 라이브러리를 사용해서 MyLogger를 상속받은 가짜 프록시 객체를 생성하고 주입한 뒤 요청이 들어오면 내부에서 진짜 빈을 요청하는 위임 로직을 수행.

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -14,15 +14,15 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-//    private final MyLogger myLogger;
+    private final MyLogger myLogger;
     // myLogger를 찾을 수 있는 dependency lookup 할 수 있는 provider를 주입받습니다.
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     // 뷰 화면 없이 문자 반환할 경우 @ResponseBody. 고객 요청 시 뷰 템플릿 거쳐서 렌더링 후 나가야 하는데 그러지 않고 문자 그대로 보내는 경우
     @ResponseBody
     // 자바에서 제공하는 표준 서블릿 규약으로 고객 요청 정보 Request를 받습니다.
-    public String logDemo(HttpServletRequest request) {
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requestURL = request.getRequestURI().toString();
         // 고객 요청 시 getObject() 최초 시점에 MyLogger 생성
         /* scope request인 MyLogger 클래스 생성 및 init으로 uuid 세팅
@@ -30,10 +30,12 @@ public class LogDemoController {
                 // 중복될 가능성이 거의 없는 식별자 랜덤값 생성
                 uuid = UUID.randomUUID().toString();
          */
-        MyLogger myLogger = myLoggerProvider.getObject();
+//        MyLogger myLogger = myLoggerProvider.getObject();
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
+        Thread.sleep(1000);
         logDemoService.logic("testId");
         return "OK";
     }
